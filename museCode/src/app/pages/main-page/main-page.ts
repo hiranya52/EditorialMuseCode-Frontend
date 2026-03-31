ef3387ef3285a24f69cc7054d0a7a8482a02ebac
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-03-31 23:51:06 +0530

    Integrate footer into main page layout

--- museCode/src/app/pages/main-page/main-page.ts
+++ museCode/src/app/pages/main-page/main-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [Footer],
   templateUrl: './main-page.html',
   styleUrl: './main-page.css',
 })
