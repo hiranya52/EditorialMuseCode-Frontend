3b5d819724ed4260f3a63aaf6b262843657f737a
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-04-02 21:23:33 +0530

    Implement Editorial Muse styled login page with immersive UI and responsive design

--- museCode/src/app/app.routes.ts
+++ museCode/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { MainPage } from './pages/main-page/main-page';
+import { LogIn } from './pages/log-in/log-in';
 
 export const routes: Routes = [
 
   {
     path: '',
     component:MainPage
+  },
+  {
+    path: 'logIn',
+    component: LogIn
   }
 
 ];
