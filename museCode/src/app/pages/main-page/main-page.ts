19404d2f1e2483245f3a82eb413526d38026ed48
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-04-03 23:43:24 +0530

    Add main page layout with navbar, sidebar, feed, and footer components

--- museCode/src/app/pages/main-page/main-page.ts
+++ museCode/src/app/pages/main-page/main-page.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { Sidebar } from "../../components/sidebar/sidebar";
+import { Navbar } from "../../components/navbar/navbar";
+import { Feed } from "../../components/feed/feed";
+import { Footer } from "../../components/footer/footer";
+import { MobileNav } from "../../components/mobile-nav/mobile-nav";
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [Sidebar, Navbar, Feed, Footer, MobileNav],
   templateUrl: './main-page.html',
   styleUrl: './main-page.css',
 })
