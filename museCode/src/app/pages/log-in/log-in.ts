44c1ad817bd733f574c1d573316008c1e904d5f3
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-04-05 18:40:01 +0530

    Include custom fonts and Tailwind CDN setup in HTML

--- museCode/src/app/pages/log-in/log-in.ts
+++ museCode/src/app/pages/log-in/log-in.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
@@ -6,4 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './log-in.html',
   styleUrl: './log-in.css',
 })
-export class LogIn {}
+export class LogIn {
+
+  constructor(private router: Router) {}
+
+  goToFeed(){
+    this.router.navigate(['/onboarding/personalize-feed']);
+  }
+
+  goToSignUp(){
+    this.router.navigate(['/signUp']);
+  }
+
+
+}
