dc0825e6e3b82dcf4af163d473c259b2666f3caa
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-04-13 09:42:44 +0530

    Refactor app structure by adding Sidebar and Navbar components, and update main-page layout

--- museCode/src/app/pages/main-page/main-page.ts
+++ museCode/src/app/pages/main-page/main-page.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../../components/sidebar/sidebar";
 import { Navbar } from "../../components/navbar/navbar";
-import { Feed } from "../../components/feed/feed";
-import { Footer } from "../../components/footer/footer";
 import { MobileNav } from "../../components/mobile-nav/mobile-nav";
+import { RouterOutlet } from "../../../../node_modules/@angular/router/types/_router_module-chunk";
+
 
 @Component({
   selector: 'app-main-page',
-  imports: [Sidebar, Navbar, Feed, Footer, MobileNav],
+  imports: [Sidebar, Navbar, MobileNav, RouterOutlet],
   templateUrl: './main-page.html',
   styleUrl: './main-page.css',
 })
