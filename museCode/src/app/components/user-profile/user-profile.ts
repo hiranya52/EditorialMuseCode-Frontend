4796839c01de945cc8905d605d9b61f43eab3bd3
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-04-15 17:01:53 +0530

    Refactor user profile component: remove old implementation and add new structure with HTML, CSS, and test setup

--- museCode/src/app/components/user-profile/user-profile.ts
+++ museCode/src/app/components/user-profile/user-profile.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { Navbar } from "../../components/navbar/navbar";
-import { Sidebar } from "../../components/sidebar/sidebar";
 
 @Component({
   selector: 'app-user-profile',
-  imports: [Navbar, Sidebar],
+  imports: [],
   templateUrl: './user-profile.html',
   styleUrl: './user-profile.css',
 })
