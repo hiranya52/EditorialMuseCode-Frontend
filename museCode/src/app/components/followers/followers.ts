d2bdfedb480c14f8c2a1307db56cd2f5198b7f49
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-04-22 23:02:50 +0530

    Add Followers component with HTML, CSS, and unit tests

---
+++ museCode/src/app/components/followers/followers.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-followers',
+  imports: [],
+  templateUrl: './followers.html',
+  styleUrl: './followers.css',
+})
+export class Followers {}
