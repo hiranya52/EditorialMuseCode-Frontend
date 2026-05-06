aae3317660c632943ce52ea0487757e6a1b1a8f8
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-06 23:53:43 +0530

    Add User class with HttpClient dependency injection

--- museCode/src/app/service/user/user.ts
+++ museCode/src/app/service/user/user.ts
@@ -1,6 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
-export class User {}
+export class User {
+
+  constructor(private http: HttpClient) { }
+
+}
