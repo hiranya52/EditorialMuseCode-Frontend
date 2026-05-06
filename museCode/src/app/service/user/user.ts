a8e85dbcb3da49df61564561213fb3457403a11c
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-06 23:56:45 +0530

    Add registerUser method to User service for user registration

--- museCode/src/app/service/user/user.ts
+++ museCode/src/app/service/user/user.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_ENDPOINTS } from '../../../core/api/api-endpoints';
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +9,9 @@ import { Injectable } from '@angular/core';
 export class User {
 
   constructor(private http: HttpClient) { }
+
+  registerUser(userObj : User): Observable<User> {
+    return this.http.post<User>(API_ENDPOINTS.user.registerUser, userObj);
+  }
 
 }
