14bf4e71135651d012500457f5874e04770d4bca
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-07 22:26:46 +0530

    Add LogInUser endpoint to API_ENDPOINTS for user authentication

--- museCode/src/core/api/api-endpoints.ts
+++ museCode/src/core/api/api-endpoints.ts
@@ -1,3 +1,4 @@
+import { LogIn } from './../../app/pages/log-in/log-in';
 import { environment } from "../../environments/environment.prod"
 
 const BASE_URL = environment.apiBaseUrl;
@@ -5,7 +6,8 @@ const BASE_URL = environment.apiBaseUrl;
 export const API_ENDPOINTS = {
 
   user: {
-    registerUser: `${BASE_URL}/auth/register`
+    registerUser: `${BASE_URL}/auth/register`,
+    LogInUser: `${BASE_URL}/auth/login`
   }
 
 }
