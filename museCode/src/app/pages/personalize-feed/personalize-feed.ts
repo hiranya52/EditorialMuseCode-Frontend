0a30e50b1e28b57f872f56e581869b4181cdb26b
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-09 18:00:32 +0530

    Refactor personalize-feed component to enhance category selection functionality and improve code structure

--- museCode/src/app/pages/personalize-feed/personalize-feed.ts
+++ museCode/src/app/pages/personalize-feed/personalize-feed.ts
@@ -1,9 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-personalize-feed',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './personalize-feed.html',
   styleUrl: './personalize-feed.css',
 })
-export class PersonalizeFeed {}
+export class PersonalizeFeed {
+
+  isSelected = false;
+
+normalClass =
+  'font-label text-sm px-6 py-3 rounded-full border border-outline-variant/30 bg-surface-container-low text-on-surface-variant hover:border-primary/40 hover:text-primary transition-all duration-200';
+
+selectedClass =
+  'group relative px-6 py-3 rounded-full border-2 border-primary-container bg-surface-container-lowest text-primary font-medium transition-all duration-300 shadow-[0_0_15px_rgba(3,33,33,0.15)] flex items-center gap-2';
+
+toggleCategory() {
+  this.isSelected = !this.isSelected;
+}
+
+}
