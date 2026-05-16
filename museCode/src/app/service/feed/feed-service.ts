e4547aa57119d1509cdd2ac0ba86c24fba054fd0
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-16 16:10:52 +0530

    Refactor FeedService to include API endpoint for personalized feed submission

--- museCode/src/app/service/feed/feed-service.ts
+++ museCode/src/app/service/feed/feed-service.ts
@@ -1,9 +1,18 @@
-import { HttpContext } from '@angular/common/http';
+import { HttpClient, httpResource } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { API_ENDPOINTS } from '../../../core/api/api-endpoints';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FeedService {
+
+  constructor(private http: HttpClient){}
+
+  setFeed(feed: string[]){
+
+    return this.http.post(API_ENDPOINTS.feed.personalized, feed);
+
+  }
 
 }
