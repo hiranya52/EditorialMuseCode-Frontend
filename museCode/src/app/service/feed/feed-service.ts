71cb9e614a964533f797be6c433064c50d7b3fc4
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-17 12:05:17 +0530

    Add getAllCategories method to FeedService for fetching categories

--- museCode/src/app/service/feed/feed-service.ts
+++ museCode/src/app/service/feed/feed-service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, httpResource } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { API_ENDPOINTS } from '../../../core/api/api-endpoints';
+import { Category } from '../../../model/Category.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +10,13 @@ import { API_ENDPOINTS } from '../../../core/api/api-endpoints';
 export class FeedService {
 
   constructor(private http: HttpClient){}
+
+
+  getAllCategories(): Observable<Category> {
+
+    return this.http.get<Category>(API_ENDPOINTS.feed.getAllCategories);
+
+  }
 
   setFeed(feed: string[]){
 
