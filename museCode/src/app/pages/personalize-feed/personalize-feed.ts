34699cc5dfc528efc852c93d0df4f472a4fdb224
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-06-03 07:37:27 +0530

    Refactor category selection in PersonalizeFeed component to use category IDs and improve toggle logic

--- museCode/src/app/pages/personalize-feed/personalize-feed.ts
+++ museCode/src/app/pages/personalize-feed/personalize-feed.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FeedService } from '../../service/feed/feed-service';
+import { Category } from '../../../model/Category.model';
 
 @Component({
   selector: 'app-personalize-feed',
@@ -8,11 +9,13 @@ import { FeedService } from '../../service/feed/feed-service';
   templateUrl: './personalize-feed.html',
   styleUrl: './personalize-feed.css',
 })
-export class PersonalizeFeed {
-
-  selectedCategories: string[] = [];
+export class PersonalizeFeed implements OnInit {
 
   private feedService = inject(FeedService);
+
+  categories: Category[] = [];
+
+  selectedCategoryIds: number[] = [];
 
   normalClass =
     'font-label text-sm px-6 py-3 rounded-full border border-outline-variant/30 bg-surface-container-low text-on-surface-variant hover:border-primary/40 hover:text-primary transition-all duration-200';
@@ -20,25 +23,53 @@ export class PersonalizeFeed {
   selectedClass =
     'group relative px-6 py-3 rounded-full border-2 border-primary-container bg-surface-container-lowest text-primary font-medium transition-all duration-300 shadow-[0_0_15px_rgba(3,33,33,0.15)] flex items-center gap-2';
 
-  toggleCategory(category: string) {
-    if (this.selectedCategories.includes(category)) {
-      this.selectedCategories = this.selectedCategories.filter((c) => c !== category);
+  ngOnInit(): void {
+
+    // this.feedService.getAllCategories().subscribe({
+    //   next: (res) => {
+    //     this.categories = res;
+    //   }
+    // });
+
+    this.feedService.getAllCategories().subscribe((res =>{
+
+      // this.categories=res;
+      console.log(res);
+
+
+    }))
+
+  }
+
+  toggleCategory(categoryId: number) {
+
+    if (this.selectedCategoryIds.includes(categoryId)) {
+
+      this.selectedCategoryIds =
+        this.selectedCategoryIds.filter(id => id !== categoryId);
+
     } else {
-      this.selectedCategories.push(category);
+
+      this.selectedCategoryIds.push(categoryId);
+
     }
-  }
-
-  isSelected(category: string): boolean {
-    return this.selectedCategories.includes(category);
-  }
-
-
-  onSubmit(){
-
-    this.feedService.setFeed(this.selectedCategories);
 
   }
 
+  isSelected(categoryId: number): boolean {
+    return this.selectedCategoryIds.includes(categoryId);
+  }
 
+  onSubmit() {
+
+    // this.feedService.setFeed({
+    //   categoryIds: this.selectedCategoryIds
+    // }).subscribe({
+    //   next: (res) => {
+    //     console.log(res);
+    //   }
+    // });
+
+  }
 
 }
