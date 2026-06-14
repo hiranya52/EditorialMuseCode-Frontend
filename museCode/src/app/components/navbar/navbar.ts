923f19b7bf50a9ca5eb6c9acd5ac50ca83b66861
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-06-14 16:12:59 +0530

    Refactor Navbar component to use dynamic profile image URL

--- museCode/src/app/components/navbar/navbar.ts
+++ museCode/src/app/components/navbar/navbar.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProfileService } from '../../service/profile/profile-service';
+import { AuthService } from '../../service/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,4 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+
+  constructor(private authService: AuthService, private profileService: ProfileService) {}
+  
+
+
+
+  profileImageUrl: string = 'https://lh3.googleusercontent.com/aida-public/AB6AXuD1yRc51jYOfI4pci7wLsRLBZ2h6CHzPvuh0N1qCtf-px58wyuNSn_f08hGaSBWUa1pEBX3vPbiGOt3HdyxyzTStnMEQOQuaaBV1I9kbILKoeV5WRcvA4SZwsQmlKyk6HpQx9QchFqULNmrx-mE9MrGYoeDdn6W-gi2BvqFrRcZhPPG-0BPBsS-xpF3UJxLGJwqyENQ2Y3npMfaGTUjZH19OmeboeDZE_hgQJ66P8goDQJzNFvMb5_pCmN34HVDHybVtKq20Zohpt4';
+
+
+
+}
