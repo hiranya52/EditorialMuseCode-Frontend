03b267c7379aa2b6e954712c62aec128097d5b51
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-06-18 13:12:08 +0530

    Comment out user profile retrieval logic in Navbar component

--- museCode/src/app/components/navbar/navbar.ts
+++ museCode/src/app/components/navbar/navbar.ts
@@ -15,13 +15,13 @@ export class Navbar {
   ngOnInit() {
     const userId = this.authService.getUserId();
 
-    if (userId) {
-      this.profileService.getUserProfile(userId).subscribe(res => {
-        console.log(userId);
+    // if (userId) {
+    //   this.profileService.getUserProfile(userId).subscribe(res => {
+    //     // console.log(userId);
 
-        console.log(res);
-      });
-    }
+    //     console.log(res);
+    //   });
+    // }
   }
 
   profileImageUrl: string = 'https://lh3.googleusercontent.com/aida-public/AB6AXuD1yRc51jYOfI4pci7wLsRLBZ2h6CHzPvuh0N1qCtf-px58wyuNSn_f08hGaSBWUa1pEBX3vPbiGOt3HdyxyzTStnMEQOQuaaBV1I9kbILKoeV5WRcvA4SZwsQmlKyk6HpQx9QchFqULNmrx-mE9MrGYoeDdn6W-gi2BvqFrRcZhPPG-0BPBsS-xpF3UJxLGJwqyENQ2Y3npMfaGTUjZH19OmeboeDZE_hgQJ66P8goDQJzNFvMb5_pCmN34HVDHybVtKq20Zohpt4';
