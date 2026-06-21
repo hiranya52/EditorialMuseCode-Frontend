b2420dbc65a5018b5aab7f55e7ce5ac3a24d79fb
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-06-21 11:20:29 +0530

    Add CommonModule and RouterModule imports to Navbar component

--- museCode/src/app/components/navbar/navbar.ts
+++ museCode/src/app/components/navbar/navbar.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ProfileService } from '../../service/profile/profile-service';
 import { AuthService } from '../../service/auth/auth.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
