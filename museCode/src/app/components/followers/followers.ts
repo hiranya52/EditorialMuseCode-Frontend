c9dede0d1c256595938b56e8739124625db0f559
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-06-24 22:44:43 +0530

    etch profile data and add default avatar fallback

--- museCode/src/app/components/followers/followers.ts
+++ museCode/src/app/components/followers/followers.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../service/auth/auth.service';
+import { ProfileService } from '../../service/profile/profile-service';
+import { Profile } from '../../../model/Profile.model';
 
 @Component({
   selector: 'app-followers',
@@ -6,4 +9,55 @@ import { Component } from '@angular/core';
   templateUrl: './followers.html',
   styleUrl: './followers.css',
 })
-export class Followers {}
+export class Followers {
+  profile: Profile = {
+    id: 0,
+    userId: 0,
+    displayName: null,
+    username: null,
+    bio: 'Sharing thoughts and stories.',
+    profileImageUrl: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+    followersCount: 0,
+    followingCount: 0,
+    articlesCount: 0,
+  };
+
+  constructor(
+    private authService: AuthService,
+    private profileService: ProfileService,
+  ) {}
+
+  ngOnInit(): void {
+    const userId = this.authService.getUserId();
+
+    if (!userId) {
+      console.error('User ID is null');
+      return;
+    }
+
+    this.profileService.getUserProfile(userId).subscribe({
+      next: (data: Profile) => {
+        console.log('API Response:', data);
+
+        this.profile = {
+          id: data.id,
+          userId: data.userId,
+          displayName: data.displayName,
+          username: data.username,
+          bio: data.bio,
+          profileImageUrl:
+          data.profileImageUrl || 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+          followersCount: data.followersCount,
+          followingCount: data.followingCount,
+          articlesCount: data.articlesCount,
+        };
+
+        console.log('Updated Profile:', this.profile);
+      },
+
+      error: (err) => {
+        console.error('Profile Error:', err);
+      },
+    });
+  }
+}
